170b844949217a22e021750deb6fbcdb2fcdde65
deivisonisidoro <deivisonisidoro@gmail.com>
2022-03-12 11:14:52 -0300

    feat: add darkmode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,66 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+/* LIBS */
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function DenseAppBar() {
+
+/*MATERIAL UI */
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Switch from '@material-ui/core/Switch';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+export default function Header() {
+  const classes = useStyles();
+  
+  /* STATES */
+  
+  /*HOOKS*/
+  const dispatch = useDispatch();
+  const darkMode = useSelector(state => state.utilsReducer.darkMode);
+
+
+
+  /* FUNCTION */
+
+
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            IPCA
+    <div className={classes.root}>
+      <AppBar position="absolute" className={classes.appBar}>
+        <Toolbar className={classes.toolbar}>
+          <Typography component="h1" variant="h6"  noWrap className={classes.title}>
+            <Link href="/" color="primary" >
+             IPCA
+            </Link>
           </Typography>
+          <Switch
+            checked={darkMode}
+                  value={darkMode}
+                  color='secondary'
+                  onChange={() =>{ 
+                    dispatch({ type: 'DARK_MODE', payload: !darkMode})
+                  }}
+                  className={classes.icons}
+          />
         </Toolbar>
       </AppBar>
-    </Box>
+    </div>
   );
 }
+
+/* STYLES */
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    background:  theme.palette.background.default
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,55 @@
 import React, { useEffect } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import { useDispatch, useSelector } from 'react-redux';
 import { getValues } from '../action';
+import { Card, Typography } from '@material-ui/core';
 
-
-
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+    backgroundColor: theme.palette.background.default
+  },
+  container: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(4),
+  },
+  paper: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    overflow: 'auto',
+    flexDirection: 'column',
+    '&:hover': {
+      cursor:  'pointer',
+   },
+  },
+  fixedHeight: {
+    height: 200,
+   
+  },
+}));
 
 export default function Home() {
-  
+
+  /* STYLES */
+  const classes = useStyles();
+
   /* HOOKS */
   const dispatch = useDispatch();
   const {values} = useSelector(state => state.valuesReducer);
+  
 
-  /* FUNCTIONS */
+  
+  /* FUNCTION */
+  
   const loadValues = async()=>{
     dispatch(await getValues());
   };
@@ -21,24 +59,37 @@ export default function Home() {
     loadValues();
   // eslint-disable-next-line react-hooks/exhaustive-deps
   },[]);
-  
+
+
+
   return (
-    <div>
-      
-      {values.length > 0 ?
-        <ul>
-          {
-            values.map((item, key)=>(
-              <div key={key}>
-                <li>{item.date}</li>
-              </div>
-            ))
-          }
-        </ul>
+    <div className={classes.root}>
+
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container  maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+           
+            <Card style={{paddingRight: 20}}>
         
-        :
-        <p>array vazio</p>
-      }
+              {values.length > 0 ?
+                <ul>
+                  {
+                    values.map((item, key)=>(
+                      <div key={key}>
+                        <li><Typography color='primary'>{item.date}</Typography></li>
+                      </div>
+                    ))
+                  }
+                </ul>
+          
+                :
+                <p>array vazio</p>
+              }
+            </Card>          
+          </Grid>
+        </Container>
+      </main>
     </div>
   );
 }
